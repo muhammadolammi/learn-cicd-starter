5a20eed7081c151a42fbb9872d19bd6ef469d321
muhammadolammi <113035346+muhammadolammi@users.noreply.github.com>
2024-01-07 06:17:28 +0100

    Update main.go

--- main.go
+++ main.go
@@ -28,7 +28,6 @@ var staticFiles embed.FS
 
 func main(){
 	//lets add some test
-	//test done
 	err := godotenv.Load(".env")
 	if err != nil {
 		log.Printf("warning: assuming default configuration. .env unreadable: %v", err)
